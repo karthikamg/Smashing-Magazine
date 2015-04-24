a7480bbf7f86c8d62553a9d6fdb61a245fa8b26a
karthika <karthikam@qburst.com>
2015-04-24 13:09:59 +0530

    Certain effects and clicks added

--- js/test.js
+++ js/test.js
@@ -1,20 +1,55 @@
+
 function tosearchBottom()
 {
 	window.scrollTo(0, document.body.scrollHeight);
-	document.getElementById("menu-contents").style.display= 'none';
-	document.getElementById("search-contents").style.display= 'initial';
+
+	menu = document.getElementById("menu-contents");
+	search = document.getElementById("search-contents");
+	media = window.matchMedia("(max-width: 800px)");
+
+	// var $window = $(window);
+
+
+	// menu.style.display= 'none';
+	// search.style.display= 'initial';
+	
+
+	// if(media.matches){
+	// 	search.style.display= 'initial';
+	// }
+	// else{
+	// 	search.style.display= 'none';
+	// }	
 }
 function tomenuBottom()
 {
 	window.scrollTo(0, document.body.scrollHeight);
-	document.getElementById("search-contents").style.display= 'none';
-	document.getElementById("menu-contents").style.display= 'initial';
+
+	// search.style.display= 'none';
+	// menu.style.display= 'initial';
+	
+
+	// if(media.matches){
+	// 	menu.style.display= 'initial';
+	// }
+	// else{
+	// 	menu.style.display= 'none';
+	// 	window.alert("alert");
+	// }
 }
 
+$(window).resize(function() {
+	windowsize = window.width;
+	console.log(windowsize);
+	if(windowsize > 800)
+		window.alert('success');
+	});
 
-$('.scrollup').click(function () {
-    $("html, body").animate({
-        scrollTop: 0
-    }, 00);
-    return false;
-});
+
+// $('.scrollup').click(function () {
+//     $("html, body").animate({
+//         scrollTop: 0
+//     }, 00);
+//     return false;
+// });
+
